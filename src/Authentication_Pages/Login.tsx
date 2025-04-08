684afe9bb359817c877c41ecf9a4087ddb9c3974
pranith0789 <pranith0789@gmail.com>
2025-04-08 19:27:03 +0530

    Frontend work has been started.

---
+++ src/Authentication_Pages/Login.tsx
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+
+const Login = () => {
+  return (
+    <div className='w-screen min h-screen bg-gradient-to-b from-purple-400 to-indigo-400 items-center justify-center flex'>
+        <div className='h-3/4 w-3/4 bg-white shadow-2xl shadow-stone-700 relative'>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
