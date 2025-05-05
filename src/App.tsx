c19a4f061f03d452fdea4de5f56fae32692ae159
pranith0789 <pranith0789@gmail.com>
2025-05-05 23:03:28 +0530

    Added welcome page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Login from "./Authentication_Pages/Login"
 import SignUp from "./Authentication_Pages/SignUp"
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import WelcomePage from "./Welcome_Page/WelcomePage"
 import Main from "./AI_Agent/Main"
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<WelcomePage/>}/>
           <Route path="/Login" element={<Login/>}/>
           <Route path="/SignUp" element={<SignUp/>}/>
           <Route path="/Main" element={<Main/>}/>
